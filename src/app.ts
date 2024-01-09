b889faac0d9f55d0173274ee2b1e5a9d6c446898
pedrohenrique-23 <pedrosilva10057@gmail.com>
2024-01-08 21:04:06 -0300

    Instalado o Helmet e finalizado completamente a entrega.

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,11 @@ import express, { json } from "express";
 import { booksRouter } from "./routes/books.routers";
 import { HandleErrors } from "./errors/handleErrors.middlewares";
 import 'express-async-errors';
+import helmet from "helmet";
 
 export const app = express();
+
+app.use(helmet());
 
 app.use(json());
 
